63ee22277f77bf500d91f5179051b249f29a2a4b
george-hong <15995610151@163.com>
2025-12-07 19:14:52 +0800

    feat: 支持排除部分插件

--- packages/print/src/index.ts
+++ packages/print/src/index.ts
@@ -2,9 +2,18 @@ import Editor, { IPluginOption } from '@hprint/core';
 import { pluginsList, SelectEvent, SelectMode } from '@hprint/plugins';
 import { fabric, LengthConvert } from '@hprint/shared';
 
-const usePlugins = (editor: Editor, options?: Record<string, IPluginOption>) => {
+const usePlugins = (editor: Editor, options?: {
+    excludes: string[]
+    options: Record<string, IPluginOption>
+}) => {
+    const excludesMap = options?.excludes?.reduce((total, cur) => {
+        total[cur] = 1;
+        return total;
+    }, {} as Record<string, 1>) || {};
     pluginsList.forEach((plugin) => {
-        editor.use(plugin, options?.[plugin.pluginName])
+        // 排除部分插件
+        if (excludesMap[plugin.pluginName]) return;
+        editor.use(plugin, options?.options?.[plugin.pluginName])
     });
 };
 
