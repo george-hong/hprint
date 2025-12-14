0da34ed14cb70177a9ab8b74dd35a32932ba484b
george-hong <15995610151@163.com>
2025-12-14 14:22:20 +0800

    feat(CenterAlignPlugin): 增加新的对齐类型并完善对齐功能
    扩展对齐插件功能，新增 leftH、topV、rightH、bottomV 四种对齐类型
    重构 positionV2 方法，使用 switch 语句处理不同对齐逻辑
    添加对象修改后同步尺寸和触发修改事件的操作

--- packages/plugins/src/plugins/CenterAlignPlugin.ts
+++ packages/plugins/src/plugins/CenterAlignPlugin.ts
@@ -14,6 +14,7 @@ class CenterAlignPlugin implements IPluginTempl {
     static pluginName = 'CenterAlignPlugin';
     static apis = ['centerH', 'center', 'position', 'centerV', 'positionV2'];
     static anignType = ['centerH', 'center', 'centerV'];
+    static alignTypeV2 = ['centerH', 'center', 'centerV', 'leftH', 'topV', 'rightH', 'bottomV'];
     // public hotkeys: string[] = ['space'];
     constructor(
         public canvas: fabric.Canvas,
@@ -72,18 +73,53 @@ class CenterAlignPlugin implements IPluginTempl {
         }
     }
 
-    positionV2(element: fabric.Object, position: 'centerH' | 'center' | 'centerV') {
-        if (CenterAlignPlugin.anignType.includes(position) && element) {
+    positionV2(element: fabric.Object, position: 'centerH' | 'center' | 'centerV' | 'leftH' | 'topV' | 'rightH' | 'bottomV') {
+        if (CenterAlignPlugin.alignTypeV2.includes(position) && element) {
             const defaultWorkspace = this.canvas
                 .getObjects()
                 .find((item) => item.id === 'workspace');
             if (defaultWorkspace) {
-                if (position === 'center') {
-                    const center = defaultWorkspace.getCenterPoint();
-                    this.canvas._centerObject(element, center);
-                } else {
+                const center = defaultWorkspace.getCenterPoint();
+                switch (position) {
+                    case 'centerH':
+                        this.canvas._centerObject(
+                            element,
+                            new fabric.Point(
+                                defaultWorkspace.getCenterPoint().x,
+                                element.getCenterPoint().y
+                            )
+                        );
+                        break;
+                    case 'centerV':
+                        this.canvas._centerObject(
+                            element,
+                            new fabric.Point(
+                                element.getCenterPoint().x,
+                                defaultWorkspace.getCenterPoint().y,
+                            )
+                        );
+                        break;
+                    case 'leftH':
+                        element.set('left', 0)
+                        break;
+                    case 'topV':
+                        element.set('top', 0)
+                        break;
+                    case 'rightH':
+                        element.set('left', defaultWorkspace.get('left') + defaultWorkspace.get('width') - element.get('width'))
+                        break;
+                    case 'bottomV':
+                        element.set('top', defaultWorkspace.get('top') + defaultWorkspace.get('height') - element.get('height'))
+                        break;
+                    case 'center':
+                        this.canvas._centerObject(element, center);
+                        break;
+                    default:
+                        break;
                 }
             }
+            element.syncOriginSizeByUnit()
+            this.canvas.fire('object:modified', element);
             this.canvas.renderAll();
         }
 
