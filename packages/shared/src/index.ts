465a412794a959a8e3c3c7bdab09b1f0e908f962
george-hong <15995610151@163.com>
2026-01-08 23:26:51 +0800

    feat: 增加版本号输出字段

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -2,5 +2,8 @@ import LengthConvert from './lengthConvert';
 import mathHelper from './mathHelper';
 import utils from './utils';
 import { fabric } from 'fabric';
+import { version } from '../package.json';
+
+if (window) (window as (typeof window & { _hprintPrintVersion: string }))._hprintPrintVersion = version
 
 export { LengthConvert, mathHelper, utils, fabric };
